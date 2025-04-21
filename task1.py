6bc3aee9c35a5ac6f0a5825d049886433d43d57b
Vladsmann <lipovoy89@gmail.com>
2025-04-21 11:37:03 +0300

    v2

--- task1.py
+++ task1.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-def get_date_from_input():
-    date_str = input("Для розрахунку кількості днів між поточною та заданою датою, задайте дату у форматі РРРР-ММ-ДД: ")
+def get_days_from_today(date_str):
+    
     try:
         inp_date = datetime.strptime(date_str, "%Y-%m-%d").date()
         today = datetime.today().date()
         delta = (today - inp_date).days
-        print(f"Різниця складає: {delta} днів")
+        return delta
     except ValueError:
         print("Неправильний формат дати. Спробуйте ще раз.")
 
-get_date_from_input()
+print(get_days_from_today("2025-01-01"))
