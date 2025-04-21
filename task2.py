a332de3e7515e7a8c93e328fde98f208fcf07906
Vladsmann <lipovoy89@gmail.com>
2025-04-21 12:12:03 +0300

    v2

--- task2.py
+++ task2.py
@@ -1,4 +1,29 @@
 import random
 
-get_numbers_ticket = random.sample(range(1, 49), 6)
-print("Ваші лотерейні числа:", get_numbers_ticket)
+def get_numbers_ticket(min, max, quantity):
+    
+    if min < 1:  
+        return []
+    
+    if max > 1000:  
+        return []
+    
+    if quantity < min:  
+        return []
+    
+    if quantity > max:  
+        return []
+    
+    lottery_numbers = []
+    
+    all_numbers = list(range(min, max))
+    
+    lottery_numbers = random.sample(all_numbers, quantity)
+    
+    lottery_numbers.sort()
+    
+    return lottery_numbers
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
