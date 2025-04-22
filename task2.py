31bf0250b2316ab733e1c7c279e3060c613136c7
Vladsmann <lipovoy89@gmail.com>
2025-04-22 20:46:24 +0300

    v4

--- task2.py
+++ task2.py
@@ -2,13 +2,19 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     
-    if min and quantity < 1:  
+    if min < 1:  
         return []
     
-    if max and quantity > 1000:  
+    if max > 1000:  
         return []
     
-    numb_dif = max - min
+    if quantity < 1:
+        return []
+    
+    if quantity > 1000:
+        return []
+
+    numb_dif = max - min + 1
     if quantity >= numb_dif:
        return []
     
@@ -22,8 +28,20 @@ def get_numbers_ticket(min, max, quantity):
     
     return lottery_numbers
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 20, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
 
-lottery_numbers = get_numbers_ticket(10, 14, 7)
+lottery_numbers = get_numbers_ticket(10, 15, 5)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+lottery_numbers = get_numbers_ticket(10, 4, 5)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+lottery_numbers = get_numbers_ticket(10, 14, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+lottery_numbers = get_numbers_ticket(-10, 10, 5)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+lottery_numbers = get_numbers_ticket(1000, 1200, 3)
 print("Ваші лотерейні числа:", lottery_numbers)
